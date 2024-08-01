fa81c2924861f08a30438339a270f5e69cf94ef9
Tadessejemal1 <tadessejemal63@gmail.com>
2024-08-01 10:57:19 +0300

    update profile page api

--- app/api/editProfile/route.js
+++ app/api/editProfile/route.js
@@ -1,29 +1,36 @@
 import { getServerSession } from "next-auth";
-import {authOptions} from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import mongoose from "mongoose";
 import { User } from '@/app/api/models/User';
 
 export async function PUT(req) {
     try {
-        mongoose.connect(process.env.MONGO_URL);
-        console.log("Connected to MongoDB");
+        await mongoose.connect(process.env.MONGO_URL);
         const data = await req.json();
-        console.log("Requesteddd data:", data);
+        console.log(data);
         const session = await getServerSession(authOptions);
-        console.log("Session retrieved:", session);
         const email = session.user.email;
 
+        await User.updateOne({email}, data)
 
-        if ('name' in data) {
-            await User.updateOne({email}, {name:data.name})
-        }
-
-        return Response.json(true)
+        return new Response(JSON.stringify(true), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' }
+        });
     } catch (error) {
-        console.error("Error updating user:", error);
+        console.error("Error creating or updating user:", error);
         return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
             status: 500,
             headers: { 'Content-Type': 'application/json' }
         });
     }
+}
+
+export async function GET() {
+    mongoose.connect(process.env.MONGO_URL);
+    const session = await getServerSession(authOptions);
+    const email = session.user.email;
+    return Response.json(
+        await User.findOne({email})
+    );
 }
