b720279ce53a908e0d6d6e41e33cd8fc59a52eb5
Tadessejemal1 <tadessejemal63@gmail.com>
2024-08-01 10:57:40 +0300

    update database schema

--- app/api/models/User.js
+++ app/api/models/User.js
@@ -2,7 +2,11 @@ import { model, models, Schema } from "mongoose";
 
 const SALT_ROUNDS = 10;
 
+
 const UserSchema = new Schema({
+  image: {
+    type: String,
+  },
   name: {
     type: String,
   },
@@ -11,9 +15,25 @@ const UserSchema = new Schema({
     required: true,
     unique: true,
   },
+  phoneNumber: {
+    type: String,
+  },
+  streetAddress: {
+    type: String,
+  },
+  postalCode: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
   password: {
     type: String,
   },
+  admin: {type: Boolean, default: false},
 }, { timestamps: true });
 
 // UserSchema.pre("save", async function (next) {
