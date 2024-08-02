5387809b747da5da544d6cd576bbc1015d973556
Tadessejemal1 <tadessejemal63@gmail.com>
2024-08-02 12:37:16 +0300

    update profile

--- app/api/editProfile/route.js
+++ app/api/editProfile/route.js
@@ -1,36 +1,48 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import {authOptions} from "@/app/api/auth/[...nextauth]/route";
+import { User } from "@/app/api/models/User";
 import mongoose from "mongoose";
-import { User } from '@/app/api/models/User';
+import {getServerSession} from "next-auth";
 
 export async function PUT(req) {
-    try {
-        await mongoose.connect(process.env.MONGO_URL);
-        const data = await req.json();
-        console.log(data);
-        const session = await getServerSession(authOptions);
-        const email = session.user.email;
+  mongoose.connect(process.env.MONGO_URL);
+  const data = await req.json();
+  const {_id, name, image, ...otherUserInfo} = data;
 
-        await User.updateOne({email}, data)
-
-        return new Response(JSON.stringify(true), {
-            status: 200,
-            headers: { 'Content-Type': 'application/json' }
-        });
-    } catch (error) {
-        console.error("Error creating or updating user:", error);
-        return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
-            status: 500,
-            headers: { 'Content-Type': 'application/json' }
-        });
-    }
-}
-
-export async function GET() {
-    mongoose.connect(process.env.MONGO_URL);
+  let filter = {};
+  if (_id) {
+    filter = {_id};
+  } else {
     const session = await getServerSession(authOptions);
     const email = session.user.email;
-    return Response.json(
-        await User.findOne({email})
-    );
+    filter = {email};
+  }
+
+  const user = await User.findOne(filter);
+  await User.updateOne(filter, {name, image});
+  await User.findOneAndUpdate({email:user.email}, otherUserInfo, {upsert:true});
+  return Response.json(true);
+}
+
+export async function GET(req) {
+  mongoose.connect(process.env.MONGO_URL);
+
+  const url = new URL(req.url);
+  const _id = url.searchParams.get('_id');
+
+  let filterUser = {};
+  if (_id) {
+    filterUser = {_id};
+  } else {
+    const session = await getServerSession(authOptions);
+    const email = session?.user?.email;
+    if (!email) {
+      return Response.json({});
+    }
+    filterUser = {email};
+  }
+
+  const user = await User.findOne(filterUser).lean();
+  const userInfo = await User.findOne({email:user.email}).lean();
+
+  return Response.json({...user, ...userInfo});
 }
