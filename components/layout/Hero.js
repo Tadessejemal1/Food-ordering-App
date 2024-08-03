aaf8615e438334e7d7dd452658bba2ff6d14a777
Tadessejemal1 <tadessejemal63@gmail.com>
2024-08-03 13:42:14 +0300

    update home page

--- components/layout/Hero.js
+++ components/layout/Hero.js
@@ -6,15 +6,16 @@ export default function Hero() {
     <section className="hero md:mt-4">
       <div className="py-8 md:py-12">
         <h1 className="text-4xl font-semibold">
-          Everything<br />
-          is better<br />
-          with a&nbsp;
+
+          Every Meal<br />
+          is a Delight<br />
+          With Us a&nbsp;
           <span className="text-primary">
-            Pizza
+            Meal
           </span>
         </h1>
         <p className="my-6 text-gray-500 text-sm">
-          Pizza is the missing piece that makes every day complete, a simple yet delicious joy in life
+          Our app turns every meal into a special occasion, bringing you the flavors and experiences that make each day a little brighter. Enjoy the ease of ordering and the joy of savoring delicious food anytime, anywhere.
         </p>
         <div className="flex gap-4 text-sm">
           <button className="flex justify-center bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full">
