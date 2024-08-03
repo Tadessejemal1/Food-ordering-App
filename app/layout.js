5c0e499237c2efa8b2144728768ea7f63928d4ce
Tadessejemal1 <tadessejemal63@gmail.com>
2024-08-03 16:33:36 +0300

    update header pages

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
             <Header />
             {children}
             <footer className="border-t p-8 text-center text-gray-500 mt-16">
-              &copy; 2023 All rights reserved
+              &copy; 2024 All rights reserved
             </footer>
           </AppProvider>
         </main>
--- components/layout/Header.js
+++ components/layout/Header.js
@@ -38,11 +38,13 @@ export default function Header() {
   const status = session?.status;
   const userData = session.data?.user;
   let userName = userData?.name || userData?.email;
-  const {cartProducts} = useContext(CartContext);
+  const { cartProducts } = useContext(CartContext);
   const [mobileNavOpen, setMobileNavOpen] = useState(false);
+
   if (userName && userName.includes(' ')) {
     userName = userName.split(' ')[0];
   }
+
   return (
     <header>
       <div className="flex items-center md:hidden justify-between">
@@ -54,32 +56,50 @@ export default function Header() {
             <ShoppingCart />
             {cartProducts?.length > 0 && (
               <span className="absolute -top-2 -right-4 bg-primary text-white text-xs py-1 px-1 rounded-full leading-3">
-            {cartProducts.length}
-          </span>
+                {cartProducts.length}
+              </span>
             )}
           </Link>
           <button
             className="p-1 border"
-            onClick={() => setMobileNavOpen(prev => !prev)}>
-            <Bars2 />
+            onClick={() => setMobileNavOpen(prev => !prev)}
+          >
+            {mobileNavOpen ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 text-gray-800"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            ) : (
+              <Bars2 />
+            )}
           </button>
         </div>
       </div>
       {mobileNavOpen && (
         <div
           onClick={() => setMobileNavOpen(false)}
-          className="md:hidden p-4 bg-gray-200 rounded-lg mt-2 flex flex-col gap-2 text-center">
-          <Link href={'/'}>Home</Link>
-          <Link href={'/menu'}>Menu</Link>
-          <Link href={'/#about'}>About</Link>
-          <Link href={'/#contact'}>Contact</Link>
+          className="fixed inset-y-0 left-0 bg-gray-200 p-4 rounded-lg flex flex-col gap-4 text-center w-[85%] md:hidden z-100"
+          style={{ maxHeight: 'calc(100vh - 0.25rem)' }} 
+        >
+          <Link href={'/'} className="block mt-6">Home</Link>
+          <Link href={'/menu'} className="block">Menu</Link>
+          <Link href={'/#about'} className="block">About</Link>
+          <Link href={'/#contact'} className="block">Contact</Link>
           <AuthLinks status={status} userName={userName} />
         </div>
       )}
       <div className="hidden md:flex items-center justify-between">
         <nav className="flex items-center gap-8 text-gray-500 font-semibold">
           <Link className="text-primary font-semibold text-2xl" href={'/'}>
-           FoodieGo
+            FoodieGo
           </Link>
           <Link href={'/'}>Home</Link>
           <Link href={'/menu'}>Menu</Link>
@@ -92,8 +112,8 @@ export default function Header() {
             <ShoppingCart />
             {cartProducts?.length > 0 && (
               <span className="absolute -top-2 -right-4 bg-primary text-white text-xs py-1 px-1 rounded-full leading-3">
-            {cartProducts.length}
-          </span>
+                {cartProducts.length}
+              </span>
             )}
           </Link>
         </nav>
